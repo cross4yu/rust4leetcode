65a709dca99866530520c674939baa9790bf963c
AwaitWind <cross4yu@gmail.com>
2025-10-03 13:20:48 -0700

    update main

--- src/solution/0000-0099/42.Trapping Rain Water.rs
+++ src/solution/0000-0099/42.Trapping Rain Water.rs
@@ -10,8 +10,8 @@ impl Solution {
         if n <= 2 {
             return 0;
         }
-        let mut left_max = vec!(0; n);
-        let mut right_max = vec!(0; n);
+        let mut left_max = vec![0; n];
+        let mut right_max = vec![0; n];
         left_max[0] = height[0];
         for i in 1..n {
             left_max[i] = left_max[i - 1].max(height[i]);
@@ -27,11 +27,57 @@ impl Solution {
         res
     }
 
+    // Monotonic Stack
+    pub fn trap(height: Vec<i32>) -> i32 {
+        let n = height.len();
+        if n <= 2 {
+            return 0;
+        }
+        let mut res = 0;
+        let mut stack = Vec::new();
+        for (i, &v) in height.iter().enumerate() {
+            while !stack.is_empty() && height[stack[stack.len() - 1]] <= v {
+                let top = height[stack.pop().unwrap()];
+                if stack.is_empty() {
+                    break;
+                }
+                let left = stack[stack.len() - 1];
+                let left_height: i32 = height[left];
+                let current_height: i32 = v;
+                let size: i32 = left_height.min(current_height) - top;
+                res += size * ((i - left - 1) as i32);
+            }
+            stack.push(i);
+        }
+        res
+    }
 
+    // point
+    pub fn trap(height: Vec<i32>) -> i32 {
+        let n = height.len();
+        if n <= 2 {
+            return 0;
+        }
+        let mut res = 0;
+        let mut left = 0;
+        let mut right = n - 1;
+        let mut left_max = 0;
+        let mut right_max = 0;
+        while left < right {
+            left_max = left_max.max(height[left]);
+            right_max = right_max.max(height[right]);
+            if (height[left] < height[right]) {
+                res += left_max - height[left];
+                left += 1;
+            } else {
+                res += right_max - height[right];
+                right -= 1;
+            }
+        }
+        res
+    }
 }
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -43,4 +89,3 @@ impl Solution {
 // @lcpr case=end
 
  */
-
